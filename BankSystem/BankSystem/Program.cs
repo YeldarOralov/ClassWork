ef67d0cfdbe313679ddb3ddd2956c9a82004da21
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-03-19 09:48:44 +0600

    Second commit

---
+++ BankSystem/BankSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount(new ConsoleReporter())
+            {
+                FullName = "П.П.Якубович"
+            };
+            
+            account.AddSum(1000);
+            account.WithdrowSum(100);
+            Console.Read();
+        }
+    }
+}

b0eabb3f85363b5e380139e1d765def45c3e41aa
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-03-25 09:43:36 +0600

    Добавление: События

---
+++ BankSystem2/BankSystem2/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem2
+{
+    public delegate void SendMessageDelegate(string message);
+
+    public class BankAccount
+    {
+        public event SendMessageDelegate ReportEvent;
+        public string FullName { get; set; }
+        public int Sum { get; private set; } = 0;      
+
+        public void AddSum(int sum)
+        {
+            Sum += sum;
+            ReportEvent?.Invoke($"Вам начислено {sum}");
+        }
+
+        public int WithdrowSum(int sum)
+        {
+            if (sum <= Sum)
+            {
+                Sum -= sum;
+                ReportEvent?.Invoke($"У вас снято {sum}");
+                return sum;
+            }
+            ReportEvent?.Invoke($"Недостаточно средств");
+            return -1;
+        }
+    }
+}
---
+++ BankSystem2/BankSystem2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount
+            {
+                FullName = "П.П.Якубович"
+            };
+
+            var reporter = new ConsoleReporter();
+            account.ReportEvent += reporter.SendMessage;
+
+            account.AddSum(1000);
+            account.WithdrowSum(100);
+            Console.Read();
+
+            
+        }
+    }
+}

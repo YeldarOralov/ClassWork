b6ffb118ce650f7251043b89c431c98a4c8beef4
YeldarOralov <YeldarOralov@users.noreply.github.com>
2019-03-25 10:14:35 +0600

    Добавление: анонимные методы, лямбда выражения

--- BankSystem2/BankSystem2/Program.cs
+++ BankSystem2/BankSystem2/Program.cs
@@ -17,12 +17,48 @@ namespace BankSystem2
 
             var reporter = new ConsoleReporter();
             account.ReportEvent += reporter.SendMessage;
+            account.ReportEvent += BlahBlah;
+
+            //анонимный метод
+            account.ReportEvent += delegate (string text) {
+                Console.WriteLine("asas");
+                return;//можно и не писать в случае с void
+            };
+
+            //лямбда выражение - это скоращение вызова анонимного метода
+            account.ReportEvent += (mess) => Console.WriteLine(mess);
+            account.ReportEvent += (text) =>
+            {
+                Console.WriteLine(text);
+            };
+
+            var data = new List<string>
+            {
+                "Алматы",
+                "Караганда",
+                "Нур-Султан"
+            };
+            //без лямбда и синт. сахара
+            var bufferList = new List<string>();
+            foreach(var cityName in data)
+            {
+                if (cityName.ToLower().Contains("т"))
+                {
+                    bufferList.Add(cityName);
+                }
+            }
+
+            //с исп. лямбда выражениями
+            var result = data.Where(cityName=> cityName.ToLower().Contains("т"));
 
             account.AddSum(1000);
             account.WithdrowSum(100);
             Console.Read();
 
-            
+            void BlahBlah(string t)
+            {
+
+            }
         }
     }
 }
